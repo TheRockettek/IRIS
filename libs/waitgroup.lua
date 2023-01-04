06ff616a079e8a421df8eed99af4de147203a2e1
blane thompson <blane1245@hotmail.co.uk>
2023-01-04 20:33:26 +0000

    ensure thread when checking

--- libs/waitgroup.lua
+++ libs/waitgroup.lua
@@ -1,16 +1,3 @@
---[[ Modified Raising to make a simple interface ]] --
---[[ Raisin by Hugeblank
-    This code is my property, but I will let you use it so long as you don't redistribute this manager for
-    monetary gain and leave this comment block untouched. Add/remove code as you wish. Should you decide to freely
-    distribute with additional modifications, please credit yourself. :)
-
-    Raisin can be found on github at:
-    `https://github.com/hugeblank/raisin`
-
-    Demonstrations of the library can also be found at:
-    `https://github.com/hugeblank/raisin-demos`
-]]
-
 local function NewWaitGroup()
     local waitGroup = {
         threads = {}
@@ -26,7 +13,7 @@ local function NewWaitGroup()
     end
 
     waitGroup._check = function(thread, name)
-        return coroutine.status(thread.coro) == "suspended" and (thread.ev == nil or thread.ev == name)
+        return thread and coroutine.status(thread.coro) == "suspended" and (thread.ev == nil or thread.ev == name)
     end
 
     waitGroup._resume = function(thread, event)
