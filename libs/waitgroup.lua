2568df31cf76801c5026e5646a0aec0a855d64a4
blane thompson <blane1245@hotmail.co.uk>
2023-01-04 20:02:48 +0000

    try making basic waitgroup

--- libs/waitgroup.lua
+++ libs/waitgroup.lua
@@ -10,171 +10,36 @@
     Demonstrations of the library can also be found at:
     `https://github.com/hugeblank/raisin-demos`
 ]]
-local function copy(t)
-    local out = {}
-    for k, v in pairs(t) do
-        if type(v) == "table" then
-            out[k] = copy(v)
-        else
-            out[k] = v
-        end
-    end
-    return out
-end
 
-local function manager(listener)
-    local this = {}
+local function NewWaitGroup()
+    local waitGroup = {
+        threads = {}
+    }
 
-    local threads = {}
-
-    local assert = function(condition, message, level)
-        if not condition then
-            level = level or 0
-            error(message, 3 + level)
-        end
-    end
-
-    assert(type(listener) == "function")
-
-    local function sort(unsorted)
-        local sorted = {}
-        sorted[#sorted + 1] = unsorted[1]
-        for i = 1, #unsorted do
-            table.insert(sorted, unsorted[i])
-        end
-
-        return sorted
-    end
-
-    local function resume(thread, event)
-        local suc, err = coroutine.resume(thread.coro, table.unpack(event, 1, event.n))
-        assert(suc, err, 2)
-        if suc then
-            return err
-        end
-    end
-
-    local function check(thread, name)
-        return thread.enabled and coroutine.status(thread.coro) == "suspended" and
-            (thread.event == nil or thread.event == name)
-    end
-
-    local interface = function(coro, filter)
-        filter = filter or {}
-        if type(filter) == "string" then
-            filter = {
-                [filter] = true
-            }
-        end
-        assert(type(filter) == "table")
-        if #filter == 0 then
-            filter = nil
-        else
-            filter = copy(filter)
-            for i = 1, #filter do
-                filter[(filter[i])] = true
-                filter[i] = nil
-            end
-        end
-        local internal = {
-            coro = coro,
-            queue = {},
-            filter = filter,
-            enabled = true,
-            event = nil
+    waitGroup.Add = function(func)
+        local thread = {
+            coro = coroutine.create(func)
         }
-        internal.instance = {
-            state = function()
-                return internal.enabled
-            end,
-            toggle = function(value)
-                internal.enabled = value or not internal.enabled
-            end,
-            remove = function()
-                for i = 1, #threads do
-                    if threads[i] == internal then
-                        table.remove(threads, i)
-                        return true
-                    end
-                end
-                return false
-            end
-        }
-        threads[#threads + 1] = internal
-        return internal.instance
+        table.insert(waitGroup.threads, thread)
     end
 
-    this.run = function()
-        local e = {}
-
+    waitGroup.Wait = function()
         while true do
-            local s_threads = sort(threads)
-            local total = #s_threads
-            for j = 1, total do
-                local thread = s_threads[j]
-                if not thread then break end
-
-                while #thread.queue ~= 0 do
-                    if check(thread, thread.queue[1][1]) then
-                        thread.event = resume(thread, thread.queue[1])
-                    end
-                    table.remove(thread.queue, 1)
-                end
-                if check(thread, e[1]) then
-                    thread.event = resume(thread, e)
-                elseif not thread.enabled then
-                    if thread.filter and thread.filter[(e[1])] then
-                        thread.queue[#thread.queue + 1] = e
-                    elseif not thread.filter then
-                        thread.queue[#thread.queue + 1] = e
-                    end
-                end
-                if coroutine.status(thread.coro) == "dead" then
-                    for k = 1, #threads do
-                        if threads[k] == thread then
-                            table.remove(threads, k)
-                            j = j - 1
-                        end
-                    end
+            local threads = waitGroup.threads
+            for t = 1, #threads do
+                local thread = threads[t]
+                local status = coroutine.status(thread.coro)
+                if status == "suspended" then
+                    coroutine.resume(thread.coro)
+                elseif status == "dead" then
+                    table.remove(threads, t)
                     if #threads == 0 then
                         return
                     end
                 end
-                total = #s_threads
             end
-            e = table.pack(listener())
+            sleep(0)
         end
-    end
-
-    this.thread = function(func, ...)
-        assert(type(func) == "function")
-        return interface(coroutine.create(func), ...)
-    end
-
-    this.group = function(...)
-        local subman = manager(listener)
-        local ii = interface(...)
-        ii.run = subman.run
-        ii.thread = subman.thread
-        ii.group = subman.group
-        return ii
-    end
-
-    return this
-end
-
-local function NewWaitGroup()
-    local raisin = manager(os.pullEvent)
-    local waitGroup = {
-        group = raisin.group()
-    }
-
-    waitGroup.Add = function(func)
-        waitGroup.group.thread(func)
-    end
-
-    waitGroup.Wait = function()
-        raisin.run()
     end
 
     return waitGroup
