4e94eaae2e4dda3b09f60a51b7331dff200bffc3
blane thompson <blane1245@hotmail.co.uk>
2023-01-04 20:30:24 +0000

    i get coroutines now...

--- libs/waitgroup.lua
+++ libs/waitgroup.lua
@@ -18,30 +18,41 @@ local function NewWaitGroup()
 
     waitGroup.Add = function(func)
         local thread = {
-            coro = coroutine.create(func)
+            coro = coroutine.create(func),
+            ev = nil
         }
+
         table.insert(waitGroup.threads, thread)
     end
 
+    waitGroup._check = function(thread, name)
+        return coroutine.status(thread.coro) == "suspended" and (thread.ev == nil or thread.ev == name)
+    end
+
+    waitGroup._resume = function(thread, event)
+        local success, result = coroutine.resume(thread.coro, table.unpack(event, 1, event.n))
+        assert(success, result)
+        return result
+    end
+
     waitGroup.Wait = function()
-        print("Wait!")
+        local e = {}
+
         while true do
             local threads = waitGroup.threads
             for t = 1, #threads do
-                local status = coroutine.status(threads[t].coro)
-                print(t, status)
-                if status == "suspended" then
-                    print(coroutine.resume(threads[t].coro))
-                elseif status == "dead" then
-                    print("Dead " .. t)
+                local thread = threads[t]
+                if waitGroup._check(thread, e[1]) then
+                    thread.ev = waitGroup._resume(thread, e)
+                end
+                if coroutine.status(thread.coro) == "dead" then
                     table.remove(threads, t)
-                    if #threads <= 1 then
-                        print("Done :)")
+                    if #threads == 0 then
                         return
                     end
                 end
             end
-            print(#threads)
+            e = table.pack(os.pullEvent())
         end
     end
 
