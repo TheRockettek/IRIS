083b0c0eff3b65401728dba88bee38d650274dd8
blane thompson <blane1245@hotmail.co.uk>
2023-01-04 22:08:07 +0000

    add logging

--- core/scanner.lua
+++ core/scanner.lua
@@ -15,45 +15,49 @@ local function ScanInventory(inventories, wg, iris, name)
         wg = waitgroup.NewWaitGroup()
     end
 
-    for i = 1, inventorySize, 1 do
-        wg.Add(function()
-            local item = peripheral.call(name, "getItemDetail", i)
+    if inventorySize then
+        for i = 1, inventorySize, 1 do
+            wg.Add(function()
+                local item = peripheral.call(name, "getItemDetail", i)
 
-            if inventories[name] == nil then
-                inventories[name] = {
-                    totalSlots = inventorySize,
-                    usedSlots = 0,
-                    totalItems = 0,
-                    itemMaxCount = 0,
-                    items = {},
-                }
-            end
+                if inventories[name] == nil then
+                    inventories[name] = {
+                        totalSlots = inventorySize,
+                        usedSlots = 0,
+                        totalItems = 0,
+                        itemMaxCount = 0,
+                        items = {},
+                    }
+                end
 
 
-            if item then
-                inventories[name].items[tostring(i)] = {
-                    name = item.name,
-                    count = item.count,
+                if item then
+                    inventories[name].items[tostring(i)] = {
+                        name = item.name,
+                        count = item.count,
 
-                    display = item.displayName,
-                    max = item.maxCount,
-                    nbt = item.nbt,
-                    tags = item.tags,
-                }
-                inventories[name].usedSlots = inventories[name].usedSlots + 1
-                inventories[name].totalItems = inventories[name].totalItems + item.count
-                inventories[name].itemMaxCount = inventories[name].itemMaxCount + item.maxCount
-            else
-                inventories[name].itemMaxCount = inventories[name].itemMaxCount + 64
-            end
-        end)
+                        display = item.displayName,
+                        max = item.maxCount,
+                        nbt = item.nbt,
+                        tags = item.tags,
+                    }
+                    inventories[name].usedSlots = inventories[name].usedSlots + 1
+                    inventories[name].totalItems = inventories[name].totalItems + item.count
+                    inventories[name].itemMaxCount = inventories[name].itemMaxCount + item.maxCount
+                else
+                    inventories[name].itemMaxCount = inventories[name].itemMaxCount + 64
+                end
+            end)
+        end
+
+        if internalWaitGroup then
+            wg.Wait(20)
+        end
+    else
+        iris.logger.Warn().Str("name", name).Msg("Failed to get size of inventory")
     end
 
-    if internalWaitGroup then
-        wg.Wait(20)
-    end
-
-    return inventorySize > 0
+    return inventorySize ~= nil and inventorySize > 0
 end
 
 local function ScanAllInventories(iris)
