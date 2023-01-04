3b6a5491172551a568d008c7d542335fd56eed9e
blane thompson <blane1245@hotmail.co.uk>
2023-01-04 22:46:04 +0000

    update bootstrapper dependencies

--- iris.lua
+++ iris.lua
@@ -1,6 +1,6 @@
-local logging = require("libs.logging")
-local irisAPI = require("irisAPI")
-local gui = require("gui.main")
+local logging = require("current.libs.logging")
+local irisAPI = require("current.irisAPI")
+local gui = require("current.gui.main")
 
 local logger = logging.NewLogger("-", "iris.log")
 logger.setLevel("trace")
