b92965a9ab3145482107d08cfd1faf22a13461d9
blane thompson <blane1245@hotmail.co.uk>
2023-01-08 17:27:10 +0000

    change default logging level to info

--- iris/bootstrap.lua
+++ iris/bootstrap.lua
@@ -2,7 +2,7 @@ local logging = require "libs.logging"
 local core = require "core"
 
 local logger = logging.NewLogger("-", "iris.log")
-logger.setLevel("trace")
+logger.setLevel("info")
 
 local iris = core.NewIRIS(logger)
 
